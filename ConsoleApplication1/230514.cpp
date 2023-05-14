7f1eff60a01470547935bb42fabab4fa2098f80c
GameSW <GameSW@7-K-04>
2023-05-14 16:58:51 +0900

    초기 커밋.

---
+++ ConsoleApplication1/230514.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream> // 헤더 파일
+
+int main() // main 함수 : 프로그램의 시작과 끝을 의미합니다.
+{
+    // 메모하기 (주석)
+    std::cout << "Hello World!\n";
+
+    // 변수
+    int userId; // int형 데이터를 저장하고 공간을 확보하고 그 공간의 이름을 userId로 지정
+}
+
+// (소괄호)    {중괄호}   [대괄호]
+// : 콜론     ; 세미콜론
